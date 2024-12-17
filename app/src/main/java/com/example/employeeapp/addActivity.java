a83185e30cc2fe7357b025faa729b5d099712bb9
fahmitha-ayub <fahmithaayub@gmail.com>
2024-12-17 10:09:09 +0530

    images Uploaded

--- app/src/main/java/com/example/employeeapp/addActivity.java
+++ app/src/main/java/com/example/employeeapp/addActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,13 +15,16 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class addActivity extends AppCompatActivity {
     Button b1,b2;
-    EditText t1,t2;
+    EditText t1,t2,t3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_add);
 
+        t1=(EditText) findViewById(R.id.ecode);
+        t2=(EditText) findViewById(R.id.password);
+        t3=(EditText) findViewById(R.id.salary);
 
         b1=(Button) findViewById(R.id.addempbut);
         b2=(Button) findViewById(R.id.backmenubut);
@@ -30,6 +34,13 @@ public class addActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent ob4=new Intent(getApplicationContext(),menuActivity.class);
                 startActivity(ob4);
+            }
+        });
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(getApplicationContext(),"Successful",Toast.LENGTH_LONG).show();
             }
         });
     }
